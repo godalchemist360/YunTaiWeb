bda2571a78fd1acf66f497c0057ca19b7e258e24
javayhu <javayhu@gmail.com>
2025-06-25 00:29:28 +0800

    chore: invoke function from code in inngest

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -4,7 +4,9 @@ export const helloWorld = inngest.createFunction(
   { id: 'hello-world' },
   { event: 'test/hello.world' },
   async ({ event, step }) => {
+    console.log('Hello World function start');
     await step.sleep('wait-a-moment', '1s');
+    console.log('Hello World function end');
     return { message: `Hello ${event.data.email}!` };
   }
 );
