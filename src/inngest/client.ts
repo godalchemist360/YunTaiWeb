79857698717d25b11bafb61312fbc029ec57bae8
javayhu <javayhu@gmail.com>
2025-08-10 11:07:10 +0800

    refactor: remove inngest

--- src/inngest/client.ts
+++
@@ -1,8 +0,0 @@
-import { Inngest } from 'inngest';
-
-/**
- * Create a client to send and receive events
- *
- * https://www.inngest.com/docs/getting-started/nextjs-quick-start
- */
-export const inngest = new Inngest({ id: 'mksaas-template' });
